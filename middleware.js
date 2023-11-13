d44a32dc9574c3bd3903a4f0b13575c876539001
Sairaj Aftab <sairajaftab6@gmail.com>
2023-11-14 00:25:40 +0600

    update middleware code

--- middleware.js
+++ middleware.js
@@ -1,5 +1,19 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
+// export const config = {
+//   matcher: ["/profile/:path*", "/upload", "/liked_videos"],
+// };
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  let cookie = request.cookies.get("next-auth.session-token");
+  if (!request.cookies.has("next-auth.session-token")) {
+    return NextResponse.redirect(new URL("/sign", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
 export const config = {
   matcher: ["/profile/:path*", "/upload", "/liked_videos"],
 };
